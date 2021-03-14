921f7f33f62a94e8a51e4597d61dfe9ad3e551be
pjoters1 <78157651+pjoters1@users.noreply.github.com>
2021-03-14 19:45:36 +0100

    Delete heislabv2 directory

--- heislabv2/Lab 2 - Heis/skeleton_project/source/driver/buttons.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
-#include "hardware.h"
-
--- heislabv2/Lab 2 - Heis/skeleton_project/source/driver/elevator.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
-#include "hardware.h"
-#include "driver/timer.h"
-
-int get_floor();
-
-int get_direction();
